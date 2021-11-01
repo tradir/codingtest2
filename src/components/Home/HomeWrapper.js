c0d33eb5ec035acfe73d74a0f0ce0d38299c3d1c
dydwns2441 <heoyjun6@gmail.com>
2021-11-01 18:08:25 +0900

    ⚡️  home 스타일, 버튼 루트 완료

--- src/components/Home/HomeWrapper.js
+++ src/components/Home/HomeWrapper.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import {
+  Container,
+  Wrapper,
+  Logo,
+  HomeDesc,
+  HomeBtnWrapper,
+  Button,
+} from './StyledHome'
+import { useHistory } from 'react-router-dom'
 
 const HomeWrapper = () => {
+  const history = useHistory()
+
   return (
-    <div>
-      home  입니다
-    </div>
+    <Container>
+      <Wrapper home="home">
+        <Logo>All Good Beer</Logo>
+        <HomeDesc>나한테 맞는 맥주 찾기</HomeDesc>
+        <HomeDesc>
+          맥주의 IBU, ABU, SRM에 따라 맛있는 맥주를 찾아보세요
+        </HomeDesc>
+        <HomeBtnWrapper>
+          <Button type="button" onClick={() => history.push('/bearlist')}>
+            Beer List에서
+            <br /> 나만의 맥주 찾기
+          </Button>
+          <Button type="button" onClick={() => history.push('/cart')}>
+            내가 담은 맥주
+            <br /> 보러 가기
+          </Button>
+        </HomeBtnWrapper>
+      </Wrapper>
+    </Container>
   )
 }
 
--- src/components/Home/StyledHome.js
+++ src/components/Home/StyledHome.js
@@ -1,1 +1,62 @@
-import styled from "styled-components";
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  height: 90vh;
+  background-color: #eff2f8;
+`
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  padding: 40px 0;
+`
+
+export const Logo = styled.h1`
+  font-size: 40px;
+  margin-bottom: 24px;
+
+  @media screen and (min-width: 670px) {
+    font-size: 72px;
+    margin-bottom: 32px;
+  }
+`
+
+export const HomeDesc = styled.p`
+  font-size: 20px;
+  margin-bottom: 10px;
+
+  @media screen and (min-width: 670px) {
+    font-size: 32px;
+    margin-bottom: 20px;
+  }
+`
+
+export const HomeBtnWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media screen and (min-width: 670px) {
+    flex-direction: row;
+  }
+`
+
+export const Button = styled.button`
+  margin: 20px;
+  width: 300px;
+  height: 250px;
+  padding: 30px 20px;
+  background-color: #fff;
+  border: none;
+  font-size: 32px;
+  line-height: 3rem;
+  border-radius: 12px;
+  cursor: pointer;
+
+  @media screen and (min-width: 670px) {
+    margin: 20px;
+    width: 300px;
+  }
+`
